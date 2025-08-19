f7c36dadb00c5f595577f6f096579693dd8ff7dd
Davico <cchadavid12@gmail.com>
2025-08-19 12:21:49 -0500

    Creacion de odelos proyecto y gastos y la migracion a la DB

--- sistema_gastos_backend/config/settings.py
+++ sistema_gastos_backend/config/settings.py
@@ -1,6 +1,13 @@
 from decouple import config
 import dj_database_url
 
+INSTALLED_APPS = [
+    ...,
+    'rest_framework',
+    'core',
+]
+
 DATABASES = {
     'default': dj_database_url.parse(config('DATABASE_URL'))
 }
+
--- sistema_gastos_backend/core/models.py
+++ sistema_gastos_backend/core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Proyectos(models.Model):
+    nombre = models.CharField(max_length=100)
+    ubicacion = models.CharField(max_length=200)
+    presupuesto = models.DecimalField(max_digits=12, decimal_places=2)
+    precio_venta = models.DecimalField(max_digits=12, decimal_places=2)
+    inversionistas = models.TextField()
+    descripcion = models.TextField()
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField(null=True, blank=True)
+    numero_agua = models.CharField(max_length = 50)
+    numero_luz = models.CharField(max_length = 50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Gastos(models.Model):
+    proyecto = models.ForeignKey(Proyectos, on_delete=models.CASCADE, related_name='gastos')
+    fecha = models.DateField()
+    categoria = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    proveedor = models.CharField(max_length=100)
+    cantidad = models.IntegerField()
+    precio_unitario = models.DecimalField(max_digits=12, decimal_places=2)
+    total = models.DecimalField(max_digits=12, decimal_places=2)
+    metodo_pago = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.categoria} - {self.total}"
